6024376b7ef0338d991e64ed4aac0a005d193309
roberthsu2003 <roberthsu2003@gmail.com>
2026-07-15 21:17:55 +0800

    Update practice2.py

--- lesson9/practice2.py
+++ lesson9/practice2.py
@@ -1,10 +1,13 @@
 from playwright.sync_api import sync_playwright
+from playwright.sync_api import Browser, Page
 
 def main():
   with sync_playwright() as p:
-    browser = p.chromium.launch()
-    
-
+    browser: Browser = p.chromium.launch(headless=False)
+    page: Page= browser.new_page()
+    page.goto("https://www.google.com")
+    print(page.title())
+    browser.close()
 
 if __name__ == "__main__":
   main()
