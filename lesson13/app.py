ea819bd1c2e421a43a48dd33cf12b338ce023f1c
roberthsu2003 <roberthsu2003@gmail.com>
2026-07-29 20:57:20 +0800

    modify

--- lesson13/app.py
+++ lesson13/app.py
@@ -1,7 +1,8 @@
-from playwright.sync_api import Locator, sync_playwright
-
+from playwright.sync_api import sync_playwright
 import crawler
+
 COOKIES_FILE = "thsrc_cookies.json"
+
 if __name__ == "__main__":
   with sync_playwright() as p:
     crawler.crawl(p=p,cookies_file=COOKIES_FILE)
